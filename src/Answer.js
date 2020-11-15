252f8d5c028c533f68b91c059a91d10aec90f609
Elijah Simpson <elijahsimpson1991@gmail.com>
2020-11-15 13:54:39 -0500

    feat: init commit

---
+++ src/Answer.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+
+// If you are confused about any of this code, go to https://www.youtube.com/watch?v=XLJN4JfniH4 for an explinaion
+
+const MyContext = React.createContext();
+class MyProvider extends Component {
+  state = {
+    name: "Obi Wan Kenobi",
+    age: 57,
+    role: "Jedi Master",
+  };
+  render() {
+    return (
+      <MyContext.Provider
+        value={{
+          state: this.state,
+          increaseAge: () => this.setState({ age: this.state.age + 1 }),
+        }}
+      >
+        {this.props.children}
+      </MyContext.Provider>
+    );
+  }
+}
+
+const Group = (props) => (
+  <div className="group">
+    <Person />
+  </div>
+);
+
+class Person extends Component {
+  render() {
+    return (
+      <ul className="person">
+        <MyContext.Consumer>
+          {(context) => (
+            <React.Fragment>
+              <li>
+                {context.state.name} - {context.state.age} -{" "}
+                {context.state.role}
+              </li>
+              <button onClick={context.increaseAge}>Increase Age</button>
+            </React.Fragment>
+          )}
+        </MyContext.Consumer>
+      </ul>
+    );
+  }
+}
+
+export default class Answer extends Component {
+  render() {
+    return (
+      <MyProvider>
+        <div>
+          <Group />
+        </div>
+      </MyProvider>
+    );
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+// REACT CONTEXT CHALLENGE
+// 1 - The code right now shows a simple React app that displays value with in a state. It is being passed down through multiple componets,
+// which is resulting in prop drilling. Your task is to refactor this app using Context.
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//2 - The result at the end should render the name,age,and role that is within the context. You can display this as either list items or paragraphs.
+
+//3 - BONUS CHALLENGE - Create a button that when pressed increses the age value by 1.
+
+//HINTS - Remeber that we need to create a React.createContext() and use .Provider to provide the date from the context and .Consumer to use the data from it
+
+// If you get stuck, the Answer.js file provides not only the answer but a link to a YouTube video to explain what everything does.
+
+import React, { Component } from "react";
+import "./App.css";
+
+const Group = (props) => (
+  <div className="group">
+    <Person name={props.name} age={props.age} role={props.role} />
+  </div>
+);
+class Person extends Component {
+  render() {
+    return (
+      <ul className="person">
+        <li>
+          {this.props.name} - {this.props.age} - {this.props.role}
+        </li>
+      </ul>
+    );
+  }
 }
 
-export default App;
+export default class App extends Component {
+  state = {
+    name: "Obi Wan Kenobi",
+    age: 57,
+    role: "Jedi Master",
+  };
+
+  render() {
+    return (
+      <div>
+        <Group
+          name={this.state.name}
+          age={this.state.age}
+          role={this.state.role}
+        />
+      </div>
+    );
+  }
+}
